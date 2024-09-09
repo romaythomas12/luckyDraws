f4b9d25cb285765b48ff77567f80670e4f0fb5af
Thomas Romay <romaythomas@gmail.com>
2024-09-09 15:37:12 +0100

    Initial Commit

---
+++ LuckyDraws/LuckyDrawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyDrawsApp.swift
+//  LuckyDraws
+//
+//  Created by Thomas Romay on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LuckyDrawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuckyDrawsTests/LuckyDrawsTests.swift
@@ -0,0 +1,16 @@
+//
+//  LuckyDrawsTests.swift
+//  LuckyDrawsTests
+//
+//  Created by Thomas Romay on 09/09/2024.
+//
+
+import Testing
+
+struct LuckyDrawsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
